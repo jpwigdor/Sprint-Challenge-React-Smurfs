072104c4d114430b031b79df3d29f1ccc1961a69
James Wigdor <jpwigdor@gmail.com>
2019-04-07 21:46:47 -0400

    Add a POST request
    names now show up properly on the server preview and within the actual local database.

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,12 +14,22 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-
-    this.setState({
-      name: "",
-      age: "",
-      height: ""
-    });
+    if (this.state.name)
+      axios
+        .post("http://localhost:3333/smurfs", {
+          name: this.state.name,
+          age: this.state.age,
+          height: this.state.height
+        })
+        .then(response => {
+          this.setState({
+            smurfs: response.data,
+            name: "",
+            age: "",
+            height: ""
+          });
+        })
+        .catch(err => console.log("Add Smurf Errored Out!", err));
   };
 
   handleInputChange = e => {
